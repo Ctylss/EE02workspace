509abb3c83f33fc9344f69fa0583d1cf90ee272e
ctyls <sometimelag@gmail.com>
2025-07-08 10:36:47 -0400

    pom.xml src/main/java/com/example/dao/util/HibernateUtil.java src/main/java/com/mes/bean/Material.java src/main/java/com/mes/dao/MaterialDAO.java src/main/resources/hibernate.cfg.xml src/main/webapp/WEB-INF/spring/app-config.xml target/classes/hibernate.cfg.xml target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst target/mywebapp/css/order.css target/mywebapp/css/orderList.css target/mywebapp/css/supplier.css target/mywebapp/JSP/zt/AddSupplier.jsp target/mywebapp/JSP/zt/EditOrder.jsp target/mywebapp/JSP/zt/OrderList.jsp target/mywebapp/JSP/zt/SupplierList.jsp target/mywebapp/WEB-INF/classes/hibernate.cfg.xml target/mywebapp/WEB-INF/spring/app-config.xml

--- src/main/java/com/mes/dao/MaterialDAO.java
+++ src/main/java/com/mes/dao/MaterialDAO.java
@@ -8,9 +8,10 @@ import org.hibernate.query.Query;
 public class MaterialDAO {
 
     // 取得所有 active = 1 的物料清單
-    public List<Material> getActiveMaterials(Session session)  {     
-        String sql = "FROM Material WHERE active = true ORDER BY materialId";
-        Query<Material> query = session.createQuery(sql,Material.class);
+    public List<Material> getActiveMaterials(Session session) {      
+        // **修改這裡：將 "FROM Material" 改為 "FROM MESMaterial"**
+        String sql = "FROM MESMaterial WHERE active = true ORDER BY materialId";
+        Query<Material> query = session.createQuery(sql, Material.class);
         return query.getResultList();
     }
 }
