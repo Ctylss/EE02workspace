8a48c46790498fbf0700f355f897231319da2857
Ctylstesttoreview <ctylswad4a86daw@gmail.com>
2025-07-08 17:18:23 +0800

    pom.xml .vscode/settings.json src/main/java/com/mes/bean/Material.java src/main/java/com/mes/bean/Order.java src/main/java/com/mes/bean/OrderItem.java src/main/java/com/mes/bean/Supplier.java src/main/java/com/mes/dao/MaterialDAO.java src/main/java/com/mes/dao/OrderDAO.java src/main/java/com/mes/dao/SupplierDAO.java src/main/java/com/mes/dao/UserDao.java src/main/java/com/mes/servlet/AddSupplierServlet.java src/main/java/com/mes/servlet/DeleteOrderServlet.java src/main/java/com/mes/servlet/DeleteSupplierServlet.java src/main/java/com/mes/servlet/EditOrderServlet.java src/main/java/com/mes/servlet/EditSupplierServlet.java src/main/java/com/mes/servlet/OrderAddFormServlet.java src/main/java/com/mes/servlet/OrderInsertServlet.java src/main/java/com/mes/servlet/OrderListServlet.java src/main/java/com/mes/servlet/SupplierListServlet.java src/main/java/com/mes/servlet/UpdateOrderServlet.java src/main/java/com/mes/servlet/UpdateSupplierServlet.java src/main/webapp/css/order.css src/main/webapp/css/orderList.css src/main/webapp/css/supplier.css src/main/webapp/JSP/zt/AddSupplier.jsp src/main/webapp/JSP/zt/EditOrder.jsp src/main/webapp/JSP/zt/OrderList.jsp src/main/webapp/JSP/zt/SupplierList.jsp target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst target/mywebapp/WEB-INF/classes/app-config.xml target/mywebapp/WEB-INF/spring/app-config.xml

--- src/main/java/com/mes/servlet/EditSupplierServlet.java
+++ src/main/java/com/mes/servlet/EditSupplierServlet.java
@@ -1,5 +1,6 @@
 package com.mes.servlet;
 
+import com.example.dao.util.HibernateUtil;
 import com.mes.bean.Supplier;
 import com.mes.dao.SupplierDAO;
 
@@ -8,45 +9,25 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Properties;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
+import org.hibernate.Session;
 
 @WebServlet("/EditSupplierServlet")
 public class EditSupplierServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private DataSource dataSource;
-
-    @Override
-    public void init() throws ServletException {
-        try {
-            // 從 context.xml 中透過 JNDI 取得資料庫連線池
-            Context initContext = new InitialContext();
-            Context envContext = (Context) initContext.lookup("java:/comp/env");
-            dataSource = (DataSource) envContext.lookup("jdbc/MESDB");
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ServletException("無法載入資料庫連線池：" + e.getMessage(), e);
-        }
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String supplierIdStr = request.getParameter("supplierId");
+        Session session = HibernateUtil.getSessionFactory().openSession();
 
         if (supplierIdStr != null && !supplierIdStr.isEmpty()) {
-        	try (Connection conn = dataSource.getConnection()) {
+        	try  {
+                session.beginTransaction();
                 int supplierId = Integer.parseInt(supplierIdStr);
                 SupplierDAO dao = new SupplierDAO();
-                Supplier supplier = dao.getSupplierById(conn, supplierId);
-
+                Supplier supplier = dao.getSupplierById(session, supplierId);
+                session.getTransaction().commit();
                 if (supplier != null) {
                     request.setAttribute("supplier", supplier);
                     request.getRequestDispatcher("/JSP/zt/EditSupplier.jsp").forward(request, response);
@@ -54,8 +35,11 @@ public class EditSupplierServlet extends HttpServlet {
                     response.getWriter().println("找不到該供應商資料");
                 }
             } catch (Exception e) {
+                session.getTransaction().rollback();
                 e.printStackTrace();
                 response.getWriter().println("發生錯誤：" + e.getMessage());
+            } finally{
+                session.close();
             }
         } else {
             response.getWriter().println("缺少供應商 ID 參數");
--- src/main/java/com/mes/servlet/UpdateOrderServlet.java
+++ src/main/java/com/mes/servlet/UpdateOrderServlet.java
@@ -1,49 +1,28 @@
 package com.mes.servlet;
 
+import com.example.dao.util.HibernateUtil;
 import com.mes.dao.OrderDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.awt.im.InputContext;
 import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Properties;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
+import org.hibernate.Session;
 
 @WebServlet("/UpdateOrderServlet")
 public class UpdateOrderServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private DataSource dataSource;
-
-    @Override
-    public void init() throws ServletException {
-        try {
-            // 從 context.xml 中透過 JNDI 取得資料庫連線池
-            Context initContext = new InitialContext();
-            Context envContext = (Context) initContext.lookup("java:/comp/env");
-            dataSource = (DataSource) envContext.lookup("jdbc/MESDB");
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ServletException("無法載入資料庫連線池：" + e.getMessage(), e);
-        }
-    }
-
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		try (Connection conn = dataSource.getConnection()) {
-			conn.setAutoCommit(false);
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try  {
+			session.beginTransaction();
 			
 			int orderId = Integer.parseInt(request.getParameter("orderId"));
 			int supplierId = Integer.parseInt(request.getParameter("supplierId"));
@@ -68,15 +47,18 @@ public class UpdateOrderServlet extends HttpServlet {
 			}
 			
 			OrderDAO dao = new OrderDAO();
-			dao.updateOrder(conn, orderId, supplierId, orderDate, orderStatus, subTotal);
-			dao.updateOrderItems(conn, orderId, materialIds, quantities, unitPrices);
+			dao.updateOrder(session, orderId, supplierId, orderDate, orderStatus, subTotal);
+			dao.updateOrderItems(session, orderId, materialIds, quantities, unitPrices);
 			
-			conn.commit();//手動提交交易，表示主檔與明細都成功寫入，資料才會正式存進資料庫。
+			session.getTransaction().commit();
 			response.sendRedirect("OrderListServlet");
 
 		}catch (Exception e) {
+				session.getTransaction().rollback();
 				e.printStackTrace();
-				 response.getWriter().println("更新失敗：" + e.getMessage());
+				response.getWriter().println("更新失敗：" + e.getMessage());
+			}finally{
+				session.close();
 			}
 //		doGet(request, response);
 	}
